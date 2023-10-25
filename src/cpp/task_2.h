079bcdadd860b8034446ceaf28b59d18dc7643e7
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-10-25 23:00:23 +0500

    Update task_2.h

--- src/cpp/task_2.h
+++ src/cpp/task_2.h
@@ -1,1 +1,27 @@
 
+
+
+#include <iostream>
+int main() {
+
+    int num;
+    int guess;
+    int tries;
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+    do{
+        std::cout << "Enter a num from  1 to 100: ";
+        std::cin >> guess;
+        tries++;
+        
+        if(guess > num){
+            std::cout << "Too high!" << std::endl;
+        } else if(guess < num){
+            std::cout << "Too Low!" << std::endl;
+        } else{
+            std::cout << "Right! Tries: " << tries << std::endl;
+        }
+    }while(guess != num);
+    
+    return 0;
+}
