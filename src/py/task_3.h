80f54b28ef5318c3b9472bbca67d34473fa456a9
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-10-25 23:21:38 +0500

    Update task_3.h

--- src/py/task_3.h
+++ src/py/task_3.h
@@ -1,1 +1,5 @@
-
+class LongArithmeticCalculator:
+    def calculate_long_arithmetic(self, divisor):
+        x = randint(10**99, 10**100 - 1)
+        print(f"{x} is a random 100-digit number\n")
+        print(float(x / divisor))
