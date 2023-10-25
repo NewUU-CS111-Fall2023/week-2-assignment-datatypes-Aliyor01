4a1d3263a5c76de3b3bdc7fe3675e076765d4343
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-10-25 23:04:25 +0500

    Update task_4.h

--- src/cpp/task_4.h
+++ src/cpp/task_4.h
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string sentence, word;
+    std::cout << "Enter a sentence: ";
+    std::getline(std::cin, sentence);
+
+    std::cout << "Enter a word to search for: ";
+    std::cin >> word;
+    size_t found = sentence.find(word);
+    if (found != std::string::npos) {
+        // Word is found, print the index of the matching substring
+        std::cout << "Match found at index: " << found << std::endl;
+    } else {
+        std::cout << "No match." << std::endl;
+    }
+
+    return 0;
+}
