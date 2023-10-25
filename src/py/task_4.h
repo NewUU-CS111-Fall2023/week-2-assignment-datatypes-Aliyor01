96600be179db1ff8deda07df6111185375318062
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-10-25 23:22:19 +0500

    Update task_4.h

--- src/py/task_4.h
+++ src/py/task_4.h
@@ -1,1 +1,6 @@
-
+class StringMatcher:
+    def match_string(self, text):
+        if text[-1] in text[0:-1]:
+            print(f'Index of "{text[-1]}" in "{" ".join(text[0:-1])}" sentence is {text[0:-1].index(text[-1])}!')
+        else:
+            print("Does not match")
